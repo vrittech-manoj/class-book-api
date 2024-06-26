50f7873f53bc42a7d0f5c2445f6a185839275f4c
vrittech-manoj <manoj@vrittechnologies.com>
2024-06-26 16:19:02 +0545

    api

--- books/viewset.py
+++ books/viewset.py
@@ -1,5 +1,8 @@
 from rest_framework import viewsets
 from .models import Books
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.filters import SearchFilter,OrderingFilter
+from django_filters.rest_framework import DjangoFilterBackend
 
 
 #******serializers class*************
@@ -14,7 +17,19 @@ class BooksSerializers(serializers.ModelSerializer):
 #***********************************
 
 
-
 class BooksViewsets(viewsets.ModelViewSet):
     queryset  = Books.objects.all()
     serializer_class = BooksSerializers
+    # permission_classes = [IsAuthenticated]
+
+    filter_backends = [SearchFilter,DjangoFilterBackend,OrderingFilter]
+
+    search_fields = ['id']
+    # ordering_fields = ['id','name']
+    filterset_fields = {
+    #    'quantity': ['exact','gte','lte'],
+        # 'price': ['exact', 'gte', 'lte'],
+    }
+
+    
+   
--- mainapp/settings.py
+++ mainapp/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'books',
+    'django_filters',
 ]
 
 MIDDLEWARE = [
